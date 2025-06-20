e94fdc19e6ee24c2624b29b730d9a67c6123113f
LBryant01 <london.bryant.lb@gmail.com>
2025-06-20 01:31:11 -0700

    Update InventoryPage.js

--- src/pages/InventoryPage.js
+++ src/pages/InventoryPage.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { fetchItems, fetchUserItems, deleteItem, updateItem, fetchItemById } from '../utils/api';
 
 const InventoryPage = ({ showUserItems = false }) => {
